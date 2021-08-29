fb670b32c2dd8a4ea6dae468695c98ca0dda012c
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-08-29 20:03:25 +0530

    Program to print table of specific number

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ namespace PrintTable
             {
                 
                 res = num * i;
-                
+                Console.WriteLine(num + " X " + i + " = " + res);
             }
             
         }
