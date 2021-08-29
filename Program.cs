184d71e0155b8a71c31c7a2f4ee6ea554110143f
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-08-29 20:02:50 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PrintTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TablePrint tp = new TablePrint();
+            tp.AccNum();
+            tp.TableCalculation();
+        }
+    }
+    public class TablePrint
+    {
+        int i, num, res;
+        public void AccNum()
+        {
+            Console.WriteLine("Enter the number :");
+            num = Convert.ToInt32(Console.ReadLine());
+        }
+        public void TableCalculation()
+        {
+            for (i = 1; i <= 10; i++)
+            {
+                
+                res = num * i;
+                
+            }
+            
+        }
+    }
+}
